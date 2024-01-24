434fdd6294849e06653295da8b289fde7bc71501
SyahrulRahmadan <syahrulbudirahmadan@gmail.com>
2024-01-24 14:35:46 +0700

    A

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -16,8 +16,39 @@ import pandas as pd
 import streamlit as st 
 
 # Import other files in the same folder
-import eda
+import eda1
+import eda2
 import prediction
 
 # Importing image, nanti tinggal di define aja di bawah
 from PIL import Image
+
+image_1 = Image.open('iFood_logo.png')
+
+
+page = st.sidebar.selectbox('Choose Your Page: ',
+                            ('Welcome Page','Data Analysis Exploration Page', 'Prediction Page', 'Model Analysis Page'))
+
+if page == 'Welcome Page':
+    st.title('Welcome to xxx')
+    st.write('')
+    st.write('Group     : FTDS 001')
+    st.write('Batch     : SBY-FTDS-002')
+    st.write('Objective :')
+    st.write('')
+    st.write('Please Select menu on the left bar')
+    st.image(image_1,caption="iFood Logo")
+    with st.expander('Background'):
+        st.caption("Di tengah pasar pengiriman makanan yang sangat kompetitif, memahami perilaku pelanggan sangat penting untuk keberhasilan kampanye pemasaran. Proyek yang kami lakukan bertujuan untuk memanfaatkan teknik-teknik data science guna mengoptimalkan strategi pemasaran sebuah perusahaan bernama iFood. Kami akan mengembangkan model klasifikasi untuk memprediksi perilaku pembelian pelanggan dalam kampanye iFood berikutnya, dan model clustering untuk mempersonalisasi pengalaman pelanggan. Dengan berfokus pada kelompok-kelompok pelanggan tertentu, iFood dapat memaksimalkan keuntungan yang dapat mereka peroleh dalam kampanye-kampanye mendatang." )
+    with st.expander('Problem Statement'):
+        st.caption("Apakah dengan membuat model machine learning dapat memperbaiki kualitas campaign untuk campaign selanjutnya")
+    with st.expander('SPONSORED BY:'):
+        st.caption("HACKTIV8")
+        st.image(image_1)
+elif page == 'Data Analysis Exploration Page':
+    eda1.run()
+elif page == 'Prediction Page':
+    prediction.run()
+else:
+    eda2.run()
+    
---
+++ Streamlit/eda2.py
@@ -0,0 +1,18 @@
+# Libraries Import
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as ps
+import seaborn as sns
+
+# Non Pre-made and Pre-made library/script importing
+from PIL import Image
+
+def run():
+    df = pd.read_csv('data_cleaned.csv')
+    
+    
+if __name__=='__main__':
+    run()
+    
